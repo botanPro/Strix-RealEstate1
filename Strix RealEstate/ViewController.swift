62bf2f451d72f12fc95fc1b262ba37f76f5a7a85
botan <botanamedi8@gmail.com>
2022-04-16 21:12:17 +0300

    Initial Commit

---
+++ Strix RealEstate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Strix RealEstate
+//
+//  Created by botan pro on 4/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
